9d58032721fe2f877d31980a770a8f0385bfecbb
Popcorn245 <bobby@madnesslabs.net>
2022-03-06 02:35:44 -0600

    fix(): Add link to projects page on home page

--- src/components/pages/home/home.tsx
+++ src/components/pages/home/home.tsx
@@ -8,6 +8,7 @@ export class PageHome {
   render() {
     return (
       <ion-content>
+        <ion-button href="/projects">Projects</ion-button>
         <ion-img src="./assets/icon/icon.png" />
       </ion-content>
     );
