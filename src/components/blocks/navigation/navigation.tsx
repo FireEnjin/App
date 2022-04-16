9d7bc092b9a035c1ca709a2242176dbb93b8599c
Popcorn245 <bobby@madnesslabs.net>
2022-04-15 23:06:39 -0500

    fix(): Change to home on navigation

--- src/components/blocks/navigation/navigation.tsx
+++ src/components/blocks/navigation/navigation.tsx
@@ -12,7 +12,7 @@ export class BlockNavigation {
     activeClassExactMatch?: boolean;
   }[] = [
     {
-      label: "Dashboard",
+      label: "Home",
       icon: "home",
       href: "/",
     },
