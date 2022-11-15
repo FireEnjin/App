4b4725f4bfb1412461fa3524b3185c892092429d
Bobby Johnson <bobby@madnesslabs.net>
2022-11-14 23:38:04 -0600

    fix(): Setup initial qwik build

--- src/helpers/pick.ts
+++
@@ -1,9 +0,0 @@
-export default function pick(sourceObject: any, keys: string[]) {
-  const newObject = {};
-  for (const key of keys) {
-    if (!sourceObject?.[key]) continue;
-    newObject[key] = sourceObject[key];
-  }
-
-  return newObject;
-}
