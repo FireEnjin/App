1cb274c736a416f71926bdd0606023eabb3a3b92
Popcorn245 <bobby@madnesslabs.net>
2023-10-16 01:54:53 -0500

    fix(): Wire up split pane for component editor

--- src/components/pages/component/component.tsx
+++ src/components/pages/component/component.tsx
@@ -1,15 +1,40 @@
 import { DatabaseService } from "@fireenjin/sdk";
-import { Build, Component, Prop, State, h } from "@stencil/core";
+import { Build, Component, Listen, Prop, State, h } from "@stencil/core";
 
 @Component({
   tag: "page-component",
   styleUrl: "component.css",
 })
 export class PageComponent {
+  leftEl: HTMLElement;
+  rightEl: HTMLElement;
+  codeEditorEl: any;
+
   @Prop() componentId: string;
   @Prop() db: DatabaseService;
 
   @State() component: any;
+  @State() mouseDown = false;
+
+  @Listen("mouseup")
+  @Listen("touchend")
+  onMouseUp() {
+    this.mouseDown = false;
+  }
+
+  @Listen("mousemove")
+  onMouseMove(event: MouseEvent) {
+    if (!this.mouseDown) return;
+    this.leftEl.style.width = `${event.clientX - 200}px`;
+    this.codeEditorEl.resize();
+  }
+
+  @Listen("touchmove")
+  onTouchMove(event: TouchEvent) {
+    if (!this.mouseDown) return;
+    this.leftEl.style.width = `${event.changedTouches[0]?.clientX - 200}px`;
+    this.codeEditorEl.resize();
+  }
 
   async componentDidLoad() {
     if (!Build?.isBrowser) return;
@@ -18,22 +43,47 @@ export class PageComponent {
 
   render() {
     return (
-      <ion-content class="ion-padding has-header">
-        <fireenjin-form
-          documentId={this.componentId}
-          endpoint={
-            !this.componentId || this.componentId === "new"
-              ? "addComponent"
-              : "editComponent"
-          }
-        >
-          <fireenjin-code-editor
-            name="code"
-            language="mdx"
-            value={this.component?.code || ""}
-            autoExpand
+      <ion-content>
+        <div class="container split">
+          <div ref={(el) => (this.leftEl = el)} class="split__left">
+            <fireenjin-form
+              documentId={this.componentId}
+              endpoint={
+                !this.componentId || this.componentId === "new"
+                  ? "addComponent"
+                  : "editComponent"
+              }
+            >
+              <fireenjin-code-editor
+                ref={(el) => (this.codeEditorEl = el)}
+                name="code"
+                language="mdx"
+                value={this.component?.code || ""}
+                autoExpand
+              />
+            </fireenjin-form>
+          </div>
+          <div
+            class="split__bar"
+            onMouseDown={() => (this.mouseDown = true)}
+            onTouchStart={() => (this.mouseDown = true)}
           />
-        </fireenjin-form>
+          <div ref={(el) => (this.rightEl = el)} class="split__right">
+            <iframe
+              style={{
+                display: "block",
+                width: "100%",
+                height: "100%",
+                position: "absolute",
+                top: "0",
+                left: "0",
+                right: "0",
+                bottom: "0",
+                pointerEvents: "none",
+              }}
+            />
+          </div>
+        </div>
       </ion-content>
     );
   }
